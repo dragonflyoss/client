d6a385bab55404990924ab45de7d46777560220f
Gaius <gaius.qi@gmail.com>
2023-06-08 16:09:15 +0800

    feat: add graceful shutdown to multi servers (#42)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- src/health/mod.rs
+++ src/health/mod.rs
@@ -14,46 +14,61 @@
  * limitations under the License.
  */
 
-use std::future::Future;
+use crate::shutdown::Shutdown;
 use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr};
+use tokio::sync::mpsc;
 use tracing::info;
 use warp::{Filter, Rejection, Reply};
 
 // Health is the health server.
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct Health {
     // addr is the address of the health server.
     pub addr: SocketAddr,
+
+    // shutdown is used to shutdown the health server.
+    shutdown: Shutdown,
+
+    // _shutdown_complete is used to notify the metrics server is shutdown.
+    _shutdown_complete: mpsc::UnboundedSender<()>,
 }
 
 // Health implements the health server.
 impl Health {
     // new creates a new Metrics.
-    pub fn new(enable_ipv6: bool) -> Self {
-        if enable_ipv6 {
-            return Self {
-                addr: SocketAddr::new(Ipv6Addr::UNSPECIFIED.into(), 40901),
-            };
-        }
+    pub fn new(
+        enable_ipv6: bool,
+        shutdown: Shutdown,
+        shutdown_complete_tx: mpsc::UnboundedSender<()>,
+    ) -> Self {
+        // Initialize the address of the server.
+        let addr = if enable_ipv6 {
+            SocketAddr::new(Ipv6Addr::UNSPECIFIED.into(), 40901)
+        } else {
+            SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), 40901)
+        };
 
         Self {
-            addr: SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), 40901),
+            addr,
+            shutdown,
+            _shutdown_complete: shutdown_complete_tx,
         }
     }
 
     // run starts the metrics server.
-    pub async fn run(&self, shutdown: impl Future) {
+    pub async fn run(&mut self) {
         let health_route = warp::path!("healthy")
             .and(warp::get())
             .and(warp::path::end())
             .and_then(Self::health_handler);
 
+        // Start the metrics server and wait for it to finish.
         tokio::select! {
             _ = warp::serve(health_route).run(self.addr) => {
                 // Health server ended.
                 info!("health server ended");
             }
-            _ = shutdown => {
+            _ = self.shutdown.recv() => {
                 // Health server shutting down with signals.
                 info!("health server shutting down");
             }
