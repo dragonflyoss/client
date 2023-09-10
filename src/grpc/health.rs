e253cada63d3810df691d6db782c0298de05e3cb
xujihui1985 <xujihui1985@gmail.com>
2023-09-10 22:34:11 +0800

    unittest: add unitest for health check
    Signed-off-by: xujihui1985 <xujihui1985@gmail.com>

--- src/grpc/health.rs
+++ src/grpc/health.rs
@@ -48,3 +48,63 @@ impl HealthClient {
         Ok(response.into_inner())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::{net::SocketAddr, time::Duration};
+
+    use tokio::task::JoinHandle;
+    use tonic::transport::Server;
+    use tonic_health::{pb::HealthCheckRequest, ServingStatus};
+
+    use crate::grpc::health::HealthClient;
+
+    struct MockHealthServer {
+        addr: String,
+        handle: JoinHandle<()>,
+    }
+
+    impl Drop for MockHealthServer {
+        fn drop(&mut self) {
+            self.handle.abort()
+        }
+    }
+
+    async fn spawn_mock_app() -> MockHealthServer {
+        let (mut health_reporter, health_service) = tonic_health::server::health_reporter();
+        health_reporter
+            .set_service_status("test", ServingStatus::Serving)
+            .await;
+
+        let addr = SocketAddr::from(([127, 0, 0, 1], 8081));
+        let handle = tokio::spawn(async move {
+            Server::builder()
+                .add_service(health_service)
+                .serve(addr)
+                .await
+                .expect("failed to start server");
+        });
+        tokio::time::sleep(Duration::from_secs(1)).await;
+        return MockHealthServer {
+            handle: handle,
+            addr: addr.to_string(),
+        };
+    }
+
+    #[tokio::test]
+    async fn test_get_avaliable_scheduler() {
+        let app = spawn_mock_app().await;
+        let health_client = HealthClient::new(&format!("http://{}", app.addr))
+            .await
+            .expect("failed to create health client");
+        let Ok(res) = health_client
+            .check(HealthCheckRequest {
+                service: String::new(),
+            })
+            .await
+        else {
+            panic!("failed to check health");
+        };
+        assert_eq!(res.status, ServingStatus::Serving as i32);
+    }
+}
