1e683e1768bccd574441e51e6054bb97f204e63f
Gaius <gaius.qi@gmail.com>
2024-04-15 12:09:52 +0800

    feat: optimize error mod
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- dragonfly-client-core/src/error/mod.rs
+++ dragonfly-client-core/src/error/mod.rs
@@ -1,3 +1,19 @@
+/*
+ *     Copyright 2024 The Dragonfly Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 pub mod errors;
 pub mod message;
 
@@ -7,6 +23,7 @@ pub use errors::ExternalError;
 pub use errors::OrErr;
 pub use errors::{DownloadFromRemotePeerFailed, HTTPError};
 
+// DFError is the error for dragonfly.
 #[derive(thiserror::Error, Debug)]
 pub enum DFError {
     // IO is the error for IO operation.
@@ -17,6 +34,7 @@ pub enum DFError {
     #[error("mpsc send: {0}")]
     MpscSend(String),
 
+    // SendTimeout is the error for send timeout.
     #[error("send timeout")]
     SendTimeout,
 
@@ -111,9 +129,11 @@ pub enum DFError {
     #[error{"unimplemented"}]
     Unimplemented,
 
+    // EmptyHTTPRangeError is the error when the range fallback error is empty.
     #[error{"RangeUnsatisfiable: Failed to parse range fallback error, please file an issue"}]
     EmptyHTTPRangeError,
 
+    // ToniceStatus is the error for tonic status.
     #[error(transparent)]
     TonicStatus(#[from] tonic::Status),
 
@@ -121,6 +141,7 @@ pub enum DFError {
     #[error(transparent)]
     HTTP(HTTPError),
 
+    // ExternalError is the error for external error.
     #[error(transparent)]
     ExternalError(#[from] ExternalError),
 }
@@ -154,8 +175,8 @@ mod tests {
         }
 
         fn do_sth_with_error() -> Result<(), DFError> {
-            function_return_inner_error().map_err(|e| {
-                ExternalError::new(crate::error::ErrorType::StorageError).with_cause(e.into())
+            function_return_inner_error().map_err(|err| {
+                ExternalError::new(crate::error::ErrorType::StorageError).with_cause(err.into())
             })?;
             Ok(())
         }
