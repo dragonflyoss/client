76aa34206f279966fb5e6ec2ffc974a21f38fd9e
imtsuki <me@qjx.app>
2024-04-23 10:45:16 +0800

    feat: extract storage engine into traits (#401)
    Signed-off-by: imtsuki <me@qjx.app>

--- dragonfly-client-storage/src/lib.rs
+++ dragonfly-client-storage/src/lib.rs
@@ -27,6 +27,8 @@ use tokio::io::AsyncRead;
 pub mod content;
 pub mod metadata;
 
+pub mod storage_engine;
+
 // DEFAULT_WAIT_FOR_PIECE_FINISHED_INTERVAL is the default interval for waiting for the piece to be finished.
 pub const DEFAULT_WAIT_FOR_PIECE_FINISHED_INTERVAL: Duration = Duration::from_millis(500);
 
