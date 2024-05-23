34dc8e69a176c7f79b94706d31a2fe9be5281421
Gaius <gaius.qi@gmail.com>
2024-05-23 13:46:26 +0800

    feat: find the available scheduler cluster by dynconfig (#491)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- dragonfly-client/src/dynconfig/mod.rs
+++ dragonfly-client/src/dynconfig/mod.rs
@@ -177,15 +177,18 @@ impl Dynconfig {
 
             // Check the health of the scheduler.
             let health_client =
-                HealthClient::new(&format!("http://{}:{}", scheduler.ip, scheduler.port))
+                match HealthClient::new(&format!("http://{}:{}", scheduler.ip, scheduler.port))
                     .await
-                    .map_err(|err| {
+                {
+                    Ok(client) => client,
+                    Err(err) => {
                         error!(
                             "create health client for scheduler {}:{} failed: {}",
                             scheduler.ip, scheduler.port, err
                         );
-                        err
-                    })?;
+                        continue;
+                    }
+                };
 
             match health_client.check().await {
                 Ok(resp) => {
