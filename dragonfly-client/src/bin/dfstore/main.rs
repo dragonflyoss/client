73b716ff241bcaf433867ab78d6f103c0d601b00
Gaius <gaius.qi@gmail.com>
2024-05-29 12:23:10 +0800

    feat: replace appender rolling with rolling file (#512)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- dragonfly-client/src/bin/dfstore/main.rs
+++ dragonfly-client/src/bin/dfstore/main.rs
@@ -57,7 +57,7 @@ struct Args {
 
     #[arg(
         long,
-        default_value_t = 3,
+        default_value_t = 6,
         help = "Specify the max number of log files"
     )]
     log_max_files: usize,
