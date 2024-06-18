9302d6647c527528146e14bea74aebc18271a604
Gaius <gaius.qi@gmail.com>
2024-06-18 17:40:16 +0800

    feat: add args to dfcache command
    Signed-off-by: Gaius <gaius.qi@gmail.com>

---
+++ dragonfly-client/src/bin/dfcache/export.rs
@@ -0,0 +1,71 @@
+/*
+ *     Copyright 2024 The Dragonfly Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use clap::Parser;
+use dragonfly_client_config::default_piece_length;
+use std::path::PathBuf;
+use std::time::Duration;
+
+// ExportCommand is the subcommand of export.
+#[derive(Debug, Clone, Parser)]
+pub struct ExportCommand {
+    #[arg(help = "Specify the cache task ID to export")]
+    id: String,
+
+    #[arg(
+        long = "application",
+        default_value = "",
+        help = "Caller application which is used for statistics and access control"
+    )]
+    application: String,
+
+    #[arg(
+        long = "tag",
+        default_value = "",
+        help = "Different tags for the same file will be divided into different cache tasks"
+    )]
+    tag: String,
+
+    #[arg(
+        long = "piece-length",
+        default_value_t = default_piece_length(),
+        help = "Specify the byte length of the piece"
+    )]
+    piece_length: u64,
+
+    #[arg(
+        short = 'O',
+        long = "output",
+        help = "Specify the output path of exporting file"
+    )]
+    output: PathBuf,
+
+    #[arg(
+        long = "timeout",
+        value_parser= humantime::parse_duration,
+        default_value = "2h",
+        help = "Specify the timeout for exporting a file"
+    )]
+    timeout: Duration,
+}
+
+// Implement the execute for ExportCommand.
+impl ExportCommand {
+    pub async fn execute(&self) -> Result<(), anyhow::Error> {
+        println!("ExportCommand is executed!");
+        Ok(())
+    }
+}
