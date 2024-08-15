983e59e1265e233862440e1782cc11a9ddab203b
Gaius <gaius.qi@gmail.com>
2024-08-15 21:05:55 +0800

    feat: add with_capacity for content
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- dragonfly-client/src/grpc/dfdaemon_upload.rs
+++ dragonfly-client/src/grpc/dfdaemon_upload.rs
@@ -791,7 +791,7 @@ impl DfdaemonUpload for DfdaemonUploadServerHandler {
             })?;
 
         // Read the content of the piece.
-        let mut content = Vec::new();
+        let mut content = Vec::with_capacity(piece.length as usize);
         reader.read_to_end(&mut content).await.map_err(|err| {
             // Collect upload piece failure metrics.
             collect_upload_piece_failure_metrics();
