2d4b93fa2117f5e3f77209e9a0e511738c8b2d3d
Gaius <gaius.qi@gmail.com>
2024-11-22 13:45:32 +0800

    fix: barrier blocks thread (#858)
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- dragonfly-client/src/bin/dfdaemon/main.rs
+++ dragonfly-client/src/bin/dfdaemon/main.rs
@@ -36,8 +36,9 @@ use dragonfly_client_storage::Storage;
 use dragonfly_client_util::id_generator::IDGenerator;
 use std::net::SocketAddr;
 use std::path::PathBuf;
-use std::sync::{Arc, Barrier};
+use std::sync::Arc;
 use tokio::sync::mpsc;
+use tokio::sync::Barrier;
 use tracing::{error, info, Level};
 
 #[cfg(not(target_env = "msvc"))]
@@ -297,24 +298,6 @@ async fn main() -> Result<(), anyhow::Error> {
 
     // Wait for servers to exit or shutdown signal.
     tokio::select! {
-        _ = {
-            let barrier = grpc_server_started_barrier.clone();
-            tokio::spawn(async move {
-                dfdaemon_upload_grpc.run(barrier).await.unwrap_or_else(|err| error!("dfdaemon upload grpc server failed: {}", err));
-            })
-        } => {
-            info!("dfdaemon upload grpc server exited");
-        },
-
-        _ = {
-            let barrier = grpc_server_started_barrier.clone();
-            tokio::spawn(async move {
-                dfdaemon_download_grpc.run(barrier).await.unwrap_or_else(|err| error!("dfdaemon download grpc server failed: {}", err));
-            })
-        } => {
-            info!("dfdaemon download grpc unix server exited");
-        },
-
         _ = tokio::spawn(async move { dynconfig.run().await }) => {
             info!("dynconfig manager exited");
         },
@@ -346,8 +329,26 @@ async fn main() -> Result<(), anyhow::Error> {
         _ = {
             let barrier = grpc_server_started_barrier.clone();
             tokio::spawn(async move {
+                dfdaemon_upload_grpc.run(barrier).await.unwrap_or_else(|err| error!("dfdaemon upload grpc server failed: {}", err));
+            })
+        } => {
+            info!("dfdaemon upload grpc server exited");
+        },
+
+        _ = {
+            let barrier = grpc_server_started_barrier.clone();
+            tokio::spawn(async move {
+                dfdaemon_download_grpc.run(barrier).await.unwrap_or_else(|err| error!("dfdaemon download grpc server failed: {}", err));
+            })
+        } => {
+            info!("dfdaemon download grpc unix server exited");
+        },
+
+        _ = {
+            let barrier = grpc_server_started_barrier.clone();
+            tokio::spawn(async move {
                 // Wait for grpc server started.
-                barrier.wait();
+                barrier.wait().await;
                 proxy.run().await.unwrap_or_else(|err| error!("proxy server failed: {}", err));
             })
         } => {
