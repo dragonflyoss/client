897a98dab8d905323f792ec5e3a5015f5a84b044
chohee <cc5281@126.com>
2025-08-28 11:43:03 +0800

    refactor: use task_id to derive key instead of constructing nonce
    - Now the pieces of one task use the same key and nonce
    - Key is derived from (the primary key, task_id), so every task uses different key
    - EncryptReader will seek corresponding offset to encrypt piece plaintext
    - When export encrypted files, we can decrypt whole file stream instead of decrypting every single piece
    
    Signed-off-by: chohee <cc5281@126.com>

--- dragonfly-client-storage/src/encrypt/algorithm/aes_ctr.rs
+++ dragonfly-client-storage/src/encrypt/algorithm/aes_ctr.rs
@@ -1,26 +1,42 @@
 use aes::Aes256;
 use ctr::Ctr128BE;
 use ctr::cipher::{StreamCipher, KeyIvInit};
+use generic_array::typenum::{U16, U32};
+// use generic_array::GenericArray;
 
 use super::EncryptionAlgorithm;
 
 pub type Aes256Ctr = Ctr128BE<Aes256>;
 
 impl EncryptionAlgorithm for Aes256Ctr {
-    const NONCE_SIZE: usize = 16;
-    const KEY_SIZE: usize = 32;
+    type NonceSize = U16;
+    type KeySize = U32;
 
-    fn new(key: &[u8], nonce: &[u8]) -> Self {
-        let key_array: [u8; Self::KEY_SIZE] = key.try_into()
-            .expect("key should be exactly 32 bytes");
-        let nonce_array: [u8; Self::NONCE_SIZE] = nonce.try_into()
-            .expect("nonce should be exactly 16 bytes");
-        
-        <Ctr128BE<Aes256> as KeyIvInit>::new(&key_array.into(), &nonce_array.into())
+    // fn new(key: &[u8], nonce: &[u8]) -> Self {
+    //     // if key.len() != <Self::KeySize as Unsigned>::to_usize() {
+    //     //     panic!("invalid key length");
+    //     // }
+    //     // if nonce.len() != <Self::NonceSize as Unsigned>::to_usize() {
+    //     //     panic!("invalid nonce length");
+    //     // }
+
+    //     let key_array = GenericArray::<u8, Self::KeySize>::from_slice(key);
+    //     let nonce_array = GenericArray::<u8, Self::NonceSize>::from_slice(nonce);
+
+    //     // <Ctr128BE<Aes256> as KeyIvInit>::new(key_array, nonce_array)
+    //     Self::new_from_array(key_array, nonce_array)
+    // }
+
+    fn new_from_array(
+            key: &generic_array::GenericArray<u8, Self::KeySize>, 
+            nonce: &generic_array::GenericArray<u8, Self::NonceSize>
+        ) -> Self where Self: Sized {
+        <Ctr128BE<Aes256> as KeyIvInit>::new(key, nonce)
     }
 
     fn apply_keystream(&mut self, data: &mut [u8]) {
         <Aes256Ctr as StreamCipher>::apply_keystream(self, data);
     }
+
 }
 
--- dragonfly-client-storage/src/encrypt/algorithm/mod.rs
+++ dragonfly-client-storage/src/encrypt/algorithm/mod.rs
@@ -1,25 +1,38 @@
 mod aes_ctr;
 
+use cipher::Unsigned;
+use generic_array::{GenericArray, ArrayLength};
+use hkdf::Hkdf;
+use sha2::Sha256;
+
 pub use aes_ctr::Aes256Ctr;
 
 pub trait EncryptionAlgorithm {
     /// Bytes of key
-    const KEY_SIZE: usize;
+    type KeySize: ArrayLength<u8>;
     /// Bytes of nonce
-    const NONCE_SIZE: usize;
+    type NonceSize: ArrayLength<u8>;
 
-    fn new(key: &[u8], nonce: &[u8]) -> Self;
+    fn new(key: &[u8], nonce: &[u8]) -> Self where Self: Sized {
+        // will panic if length is not fit
+        let key_array = GenericArray::<u8, Self::KeySize>::from_slice(key);
+        let nonce_array = GenericArray::<u8, Self::NonceSize>::from_slice(nonce);
+        Self::new_from_array(key_array, nonce_array)
+    }
+
+    fn new_from_array(
+        key: &GenericArray<u8, Self::KeySize>, 
+        nonce: &GenericArray<u8, Self::NonceSize>
+    ) -> Self where Self: Sized;
 
     fn apply_keystream(&mut self, data: &mut [u8]);
 
-    fn build_nonce(task_id: &str, piece_num: u32) -> Vec<u8> {
-        let mut nonce = vec![0u8; Self::NONCE_SIZE];
-        let task_bytes = task_id.as_bytes();
-
-        let len = std::cmp::min(12, task_bytes.len());
-        assert!(len > 0);
-        nonce[..len].copy_from_slice(&task_bytes[..len]); // first 12 bytes dor task_id
-        nonce[12..].copy_from_slice(&piece_num.to_be_bytes()); // remaining bytes for piece num
-        nonce
+    fn derive_key(master_key: &[u8], task_id: &str) -> GenericArray<u8, Self::KeySize> {
+        let hk = Hkdf::<Sha256>::new(Some(task_id.as_bytes()), master_key);
+        let mut okm: GenericArray<u8, Self::KeySize> = GenericArray::default();
+        hk.expand(b"task-encryption", &mut okm).unwrap();
+        assert_eq!(okm.len(), <Self::KeySize as Unsigned>::to_usize());
+        assert_eq!(okm.len(), master_key.len());
+        okm
     }
 }
