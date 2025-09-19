c72758bb65b75869c8f1269ac30c889f78a01a1d
Gaius <gaius.qi@gmail.com>
2025-09-19 18:02:16 +0800

    feat(config): add default download protocol for dfdaemon
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- dragonfly-client-config/src/dfdaemon.rs
+++ dragonfly-client-config/src/dfdaemon.rs
@@ -60,6 +60,12 @@ pub fn default_download_unix_socket_path() -> PathBuf {
     crate::default_root_dir().join("dfdaemon.sock")
 }
 
+/// default_download_protocol is the default protocol of downloading.
+#[inline]
+fn default_download_protocol() -> String {
+    "tcp".to_string()
+}
+
 /// default_download_request_rate_limit is the default rate limit of the download request in the
 /// download grpc server, default is 4000 req/s.
 pub fn default_download_request_rate_limit() -> u64 {
@@ -186,12 +192,6 @@ fn default_scheduler_schedule_timeout() -> Duration {
 #[inline]
 fn default_dynconfig_refresh_interval() -> Duration {
     Duration::from_secs(300)
-}
-
-/// default_storage_server_protocol is the default protocol of the storage server.
-#[inline]
-fn default_storage_server_protocol() -> String {
-    "tcp".to_string()
 }
 
 /// default_storage_server_tcp_port is the default port of the storage tcp server.
@@ -488,6 +488,10 @@ pub struct Download {
     /// server is the download server configuration for dfdaemon.
     pub server: DownloadServer,
 
+    /// protocol is the protocol of downloading from parents, support tcp and quic.
+    #[serde(default = "default_download_protocol")]
+    pub protocol: String,
+
     /// parent_selector is the download parent selector configuration for dfdaemon.
     pub parent_selector: ParentSelector,
 
@@ -518,6 +522,7 @@ impl Default for Download {
     fn default() -> Self {
         Download {
             server: DownloadServer::default(),
+            protocol: default_download_protocol(),
             parent_selector: ParentSelector::default(),
             rate_limit: default_download_rate_limit(),
             piece_timeout: default_download_piece_timeout(),
@@ -961,11 +966,6 @@ impl Default for Dynconfig {
 #[derive(Debug, Clone, Validate, Deserialize)]
 #[serde(default, rename_all = "camelCase")]
 pub struct StorageServer {
-    /// protocol is the protocol of the storage server. The protocol used for downloading pieces
-    /// between different peers, now support tcp and quic protocol.
-    #[serde(default = "default_storage_server_protocol")]
-    pub protocol: String,
-
     /// ip is the listen ip of the gRPC server.
     pub ip: Option<IpAddr>,
 
@@ -982,22 +982,9 @@ pub struct StorageServer {
 impl Default for StorageServer {
     fn default() -> Self {
         StorageServer {
-            protocol: default_storage_server_protocol(),
             ip: None,
             tcp_port: default_storage_server_tcp_port(),
             quic_port: default_storage_server_quic_port(),
-        }
-    }
-}
-
-/// StorageServer implements storage server.
-impl StorageServer {
-    /// Port returns the port of the storage server based on the protocol.
-    pub fn port(&self) -> u16 {
-        match self.protocol.as_str() {
-            "tcp" => self.tcp_port,
-            "quic" => self.quic_port,
-            _ => self.tcp_port,
         }
     }
 }
