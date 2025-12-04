32b49aea1d6a15c71e605734e193f773473fb4f8
Gaius <gaius.qi@gmail.com>
2025-12-04 17:33:39 +0800

    chore(deps): update dragonfly-client versions to 1.1.7
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- dragonfly-client/src/grpc/dfdaemon_upload.rs
+++ dragonfly-client/src/grpc/dfdaemon_upload.rs
@@ -410,7 +410,7 @@ impl DfdaemonUpload for DfdaemonUploadServerHandler {
         let download_clone = download.clone();
         let task_manager_clone = self.task.clone();
         let task_clone = task.clone();
-        let (out_stream_tx, out_stream_rx) = mpsc::channel(8);
+        let (out_stream_tx, out_stream_rx) = mpsc::channel(4);
 
         // Define the error handler to send the error to the stream.
         async fn handle_error(
@@ -1375,7 +1375,7 @@ impl DfdaemonUpload for DfdaemonUploadServerHandler {
         let request_clone = request.clone();
         let task_manager_clone = self.persistent_cache_task.clone();
         let task_clone = task.clone();
-        let (out_stream_tx, out_stream_rx) = mpsc::channel(8);
+        let (out_stream_tx, out_stream_rx) = mpsc::channel(4);
 
         // Define the error handler to send the error to the stream.
         async fn handle_error(
--- dragonfly-client/src/resource/task.rs
+++ dragonfly-client/src/resource/task.rs
@@ -568,7 +568,7 @@ impl Task {
         let mut finished_pieces: Vec<metadata::Piece> = Vec::new();
 
         // Initialize stream channel.
-        let (in_stream_tx, in_stream_rx) = mpsc::channel(8);
+        let (in_stream_tx, in_stream_rx) = mpsc::channel(4);
 
         // Send the register peer request.
         in_stream_tx
