f337bcdd65f640ac75ef52b794f5882ed2627b43
Gaius <gaius.qi@gmail.com>
2026-03-03 11:07:41 +0800

    chore(certs): regenerate test certificates with 100-year validity (#1694)
    Regenerate test certificates to extend their validity period from
    short-term (365/1024 days) to 100 years (36500 days) to prevent
    test failures caused by certificate expiration.
    
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -6,11 +6,11 @@ cd certs
 
 # Generate CA private key and self-signed cert.
 openssl genrsa -out ca.key 2048
-openssl req -x509 -new -nodes -key ca.key -sha256 -days 1024 -out ca.crt -subj "/C=CN/ST=Beijing/L=Beijing/O=Test CA/OU=IT/CN=Test CA"
+openssl req -x509 -new -nodes -key ca.key -sha256 -days 36500 -out ca.crt -subj "/C=CN/ST=Beijing/L=Beijing/O=Test CA/OU=IT/CN=Test CA"
 
 # Generate another CA private key and self-signed cert.
 openssl genrsa -out wrong-ca.key 2048
-openssl req -x509 -new -nodes -key wrong-ca.key -sha256 -days 1024 -out wrong-ca.crt -subj "/C=CN/ST=Beijing/L=Beijing/O=Wrong CA/OU=IT/CN=Wrong CA"
+openssl req -x509 -new -nodes -key wrong-ca.key -sha256 -days 36500 -out wrong-ca.crt -subj "/C=CN/ST=Beijing/L=Beijing/O=Wrong CA/OU=IT/CN=Wrong CA"
 
 # Generate OpenSSL config file with SAN extention.
 cat >san.cnf <<EOL
@@ -29,7 +29,7 @@ openssl genrsa -out server.key 2048
 openssl req -new -key server.key -out server.csr -subj "/C=CN/ST=Beijing/L=Beijing/O=Test Server/OU=IT/CN=localhost" -config san.cnf
 
 # Sign server CSR by using CA with SAN extension.
-openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 365 -sha256 -extensions v3_req -extfile san.cnf
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 36500 -sha256 -extensions v3_req -extfile san.cnf
 
 # Convert server.crt to pem.
 openssl x509 -in server.crt -out server.pem -outform PEM
@@ -39,7 +39,7 @@ openssl genrsa -out client.key 2048
 openssl req -new -key client.key -out client.csr -subj "/C=CN/ST=Beijing/L=Beijing/O=Test Server/OU=IT/CN=localhost" -config san.cnf
 
 # Sign client CSR by using CA with SAN extension.
-openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 365 -sha256 -extensions v3_req -extfile san.cnf
+openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 36500 -sha256 -extensions v3_req -extfile san.cnf
 
 # Convert client.crt to pem.
 openssl x509 -in client.crt -out client.pem -outform PEM
